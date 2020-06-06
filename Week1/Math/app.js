579db8c9d063a2cf02e1a3f491fa68c5644daeac
josephGreen19 <58785146+josephGreen19@users.noreply.github.com>
2020-06-05 22:14:45 -0400

    week1

---
+++ Week1/Math/app.js
@@ -0,0 +1,9 @@
+const {sum, subtract, mult, divide} = require("./math.js")
+//Add
+console.log(sum(20, 20))
+//Subtract
+console.log(subtract(100, 50))
+//Multiply
+console.log(mult(2, 10))
+//Divide
+console.log(divide(40, 10))
